d9ce92a197df67ff2ccbec8bfc2f118d887fec7c
Robert Schmahl <rschm007@gmail.com>
2020-11-03 14:43:15 -0800

    decided to redesign page. added typewriter introduction

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,64 @@
 $(document).ready(function () {
     
+    // typewriter function
+    var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
+
+    
     // when user hovers over nav links, they should become bold, then return to normal weight after pointer leaves
     $("h3").mouseover(function() {
         $(this).addClass("font-medium").removeClass("font-normal");
@@ -7,4 +66,7 @@ $(document).ready(function () {
     $("h3").mouseout(function() {
         $(this).addClass("font-normal").removeClass("font-medium");
     });
+
+    // portfolio menu content
+    // content 1. When user mouseovers image, convert image to grayscale and have a pop-up module that displays "graphic design" text
 });
