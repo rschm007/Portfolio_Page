f71032782fb27a77287283e29beacfd2affd2251
Robert Schmahl <rschm007@gmail.com>
2020-11-03 18:31:52 -0800

    added good typewriter function

--- assets/script.js
+++ assets/script.js
@@ -1,64 +1,58 @@
 $(document).ready(function () {
     
-    // typewriter function
-    var TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-    };
-
-    TxtType.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
-
-        if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-        var that = this;
-        var delta = 200 - Math.random() * 100;
-
-        if (this.isDeleting) { delta /= 2; }
-
-        if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-        }
-
-        setTimeout(function() {
-        that.tick();
-        }, delta);
-    };
-
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-        document.body.appendChild(css);
-    };
-
+    // defining typewrite function
+    (function ( $ ) {
+        $.fn.typewrite = function ( options ) {
+            var settings = {
+                'selector': this,
+                'extra_char': '',
+                'delay':    100,
+                'trim':     false,
+                'callback': true
+            };
+            if (options) $.extend(settings, options);
     
+            /* This extra closure makes it so each element
+             * matched by the selector runs sequentially, instead
+             * of all at the same time. */
+            function type_next_element(index) {
+                var current_element = $(settings.selector[index]);
+                var final_text = current_element.text();
+                if (settings.trim) final_text = $.trim(final_text);
+                current_element.html("").show();
+    
+                function type_next_character(element, i) {
+                    element.html( final_text.substr(0, i)+settings.extra_char );
+                    if (final_text.length >= i) {
+                        setTimeout(function() {
+                            type_next_character(element, i+1);
+                        }, settings.delay);
+                    }
+                    else {
+                        if (++index < settings.selector.length) {
+                            type_next_element(index);
+                        }
+                        else if (settings.callback) settings.callback();
+                    }
+                }
+                type_next_character(current_element, 0);
+            }
+            type_next_element(0);
+            return this;
+        };
+    })(jQuery);
+    
+    // bold text that results from typewriter function
+    $("Robert Schmahl").css("font-weight", "bold");
+    
+    // calling typewriter callback
+    $(".typewriter").typewrite({
+        // on callback, insert button under text
+        'callback': function(){
+            $()
+        }
+    })
+
     // when user hovers over nav links, they should become bold, then return to normal weight after pointer leaves
     $("h3").mouseover(function() {
         $(this).addClass("font-medium").removeClass("font-normal");
