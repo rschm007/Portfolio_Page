5def21e0bb4bb32a1b6fbcfed2d23e0e97b2ff7c
Robert Schmahl <rschm007@gmail.com>
2020-11-04 18:53:11 -0800

    added background color and changed button to icon

--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,52 @@
 $(document).ready(function () {
-    
-    // defining typewrite function
-    (function ( $ ) {
-        $.fn.typewrite = function ( options ) {
-            var settings = {
-                'selector': this,
-                'extra_char': '',
-                'delay':    70,
-                'trim':     false,
-                'callback': true
-            };
-            if (options) $.extend(settings, options);
-    
-            /* This extra closure makes it so each element
-             * matched by the selector runs sequentially, instead
-             * of all at the same time. */
-            function type_next_element(index) {
-                var current_element = $(settings.selector[index]);
-                var final_text = current_element.text();
-                if (settings.trim) final_text = $.trim(final_text);
-                // reveal letters as they are typed
-                current_element.html("").addClass("visible").removeClass("invisible");
-    
-                function type_next_character(element, i) {
-                    element.html( final_text.substr(0, i)+settings.extra_char );
-                    if (final_text.length >= i) {
-                        setTimeout(function() {
-                            type_next_character(element, i+1);
-                        }, settings.delay);
-                    }
-                    else {
-                        if (++index < settings.selector.length) {
-                            type_next_element(index);
-                        }
-                        else if (settings.callback) settings.callback();
-                    }
-                }
-                type_next_character(current_element, 0);
-            }
-            type_next_element(0);
-            return this;
-        };
-        // bold certain text that appears
-        $("div:contains('Robert')").css("font-weight", "bold");
+  // defining typewrite function
+  (function ($) {
+    $.fn.typewrite = function (options) {
+      var settings = {
+        selector: this,
+        extra_char: "",
+        delay: 70,
+        trim: false,
+        callback: true,
+      };
+      if (options) $.extend(settings, options);
 
-    })(jQuery);
-    
-    // calling typewriter callback
-    $(".typewriter").typewrite({
-        // on callback, insert button under text
-        'callback': function(){
-            $(".introduction").append(`<button class="animate-bounce bg-indigo-500 border border-indigo-800 text-base text-white font-normal mt-9 py-2 px-4 rounded shadow-lg workBtn transition duration-200 ease-in-out hover:bg-indigo-800">See My Work</button>`)
+      /* This extra closure makes it so each element
+       * matched by the selector runs sequentially, instead
+       * of all at the same time. */
+      function type_next_element(index) {
+        var current_element = $(settings.selector[index]);
+        var final_text = current_element.text();
+        if (settings.trim) final_text = $.trim(final_text);
+        // reveal letters as they are typed
+        current_element.html("").addClass("visible").removeClass("invisible");
+
+        function type_next_character(element, i) {
+          element.html(final_text.substr(0, i) + settings.extra_char);
+          if (final_text.length >= i) {
+            setTimeout(function () {
+              type_next_character(element, i + 1);
+            }, settings.delay);
+          } else {
+            if (++index < settings.selector.length) {
+              type_next_element(index);
+            } else if (settings.callback) settings.callback();
+          }
         }
-    })
+        type_next_character(current_element, 0);
+      }
+      type_next_element(0);
+      return this;
+    };
+    // bold certain text that appears
+    $("div:contains('Robert')").css("font-weight", "bold");
+  })(jQuery);
 
-
+  // calling typewriter callback
+  $(".typewriter").typewrite({
+    // on callback, insert button under text
+    callback: function () {
+      $(".introduction").append(`<i class="fas fa-chevron-circle-down animate-bounce m-10 h-12 w-12"></i>`);
+    },
+  });
 });
