d689881aa8e889a424535e1b0738c0f237e01f78
Robert Schmahl <rschm007@gmail.com>
2020-11-10 16:13:19 -0800

    skills page is finished

--- assets/script.js
+++ assets/script.js
@@ -117,5 +117,79 @@ $(document).ready(function () {
         $(".projectsLink").addClass("active");
       }, 1600);
     }
+    // properties for skills page
+    if (btnTxt === "Skills") {
+      setTimeout(function () {
+        $("main")
+          .addClass("from-orange-700 via-orange-500 to-yellow-400")
+          .removeClass(" from-indigo-900 via-blue-800 to-blue-600");
+        $("header")
+          .addClass("grid-rows-2 grid-cols-9")
+          .removeClass("introduction grid-rows-6 grid-flow-col")
+          .empty().append(`
+        <div class="row-span-1 col-span-9"></div>
+        ${nav}
+        <div class="col-span-1"></div>
+        <div class="grid grid-cols-3 col-span-7 justify-center text-blue-900">
+          <div class="skills1 col-span-1 bg-gray-100 w-auto h-auto mx-5 p-10 rounded-lg">
+            <i class="fas fa-cogs fa-4x mt-8"></i>
+            <h2 class="text-2xl font-bold mt-4 p-2">Developer</h2>
+            <p class="mt-2 p-2 mx-3">I am a full-stack web developer, able to tackle projects from front-end to back-end.</p>
+            <h3 class="text-xl text-pink-800 font-semibold mt-4 p-3">Specialties:</h3>
+            <p>UX, UI, Web, Mobile, Brands</p>
+            <h3 class="text-xl text-pink-800 font-semibold mt-2 p-3">Dev Tools & Languages:</h3>
+            <dl>
+              <dt>HTML/Pug</dt>
+              <dt>CSS/Sass</dt>
+              <dt>Javascript</dt>
+              <dt>Visual Studio Code</dt>
+              <dt>Github</dt>
+              <dt>Terminal</dt>
+              <dt>Bootstrap</dt>
+              <dt>TailwindCSS</dt>
+            </dl>
+
+          </div>
+          <div class="skills2 col-span-1 bg-gray-100 w-auto h-auto mx-5 p-10 rounded-lg">
+            <i class="fas fa-pencil-ruler fa-4x mt-8"></i>
+            <h2 class="text-2xl font-bold mt-4 p-2">Designer</h2>
+            <p class="mt-2 p-2 mx-3">I strive to create elegant, resonant designs that create impactful interactions with the user.</p>
+            <h3 class="text-xl text-pink-800 font-semibold mt-2 p-3">Things I love to design:</h3>
+            <dl>
+              <dt>UX, UI</dt>
+              <dt>Logos</dt>
+              <dt>Posters</dt>
+              <dt>Social Media Images</dt>
+              <dt>Web</dt>
+              <dt>Mobile</dt>
+              <dt>Apps</dt>
+              <dt>Storefronts</dt>
+            </dl>
+
+          </div>
+          <div class="skills3 col-span-1 bg-gray-100 w-auto h-auto mx-5 p-10 rounded-lg">
+            <i class="fas fa-users fa-4x mt-8"></i>
+            <h2 class="text-2xl font-bold mt-4 p-2">Collaborator</h2>
+            <p class="mt-2 p-2 mx-3">I love working in teams and draw upon a diverse experience background to solve problems.</p>
+            <h3 class="text-xl text-pink-800 font-semibold mt-2 p-3">Experiences I draw from:</h3>
+            <dl>
+              <dt>2+ years government experience</dt>
+              <dt>3+ years web development</dt>
+              <dt>4+ years graphic design</dt>
+              <dt>4+ years small business owner</dt>
+              <dt>4+ years event coordination</dt>
+              <dt>4+ years web design</dt>
+              <dt>6+ years photography</dt>
+              <dt>8+ years customer service</dt>
+            </dl>
+          </div>
+        </div>
+        <div class="col-span-1"></div>
+        `);
+        $("nav").addClass("col-span-9");
+        $(".skillsLink").addClass("active");
+      }, 1600);
+    }
+    
   });
 });
