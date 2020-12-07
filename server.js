39654721ec51c11c49bfcbabffb034842bb79c24
robert schmahl <rschm007@gmail.com>
2020-12-06 19:54:07 -0800

    continued work on mobile responsiveness

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require("express");
 const app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 // render static files
 app.use(express.static("public"));
